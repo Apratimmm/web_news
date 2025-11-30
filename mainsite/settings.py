c083d153d40c9213cc72746991c3226c46305416
Apratim <Aapratimboy@example.com>
2025-12-01 00:19:04 +0545

    Render deployment changes

--- mainsite/settings.py
+++ mainsite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-zo8u3b*-$8_a4s0ne)s_38rbcnnau7vy^39ad^(ev^w-lo4ejy
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ["*",]
 
 
 # Application definition
@@ -115,7 +115,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+import os
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
