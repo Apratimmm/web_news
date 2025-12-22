2a2ced7ef58ad49192ebc5feb780f1ffd71e98b3
Apratim <Aapratimboy@example.com>
2025-12-22 15:41:58 +0545

    obv new urls

--- news/urls.py
+++ news/urls.py
@@ -5,5 +5,6 @@ from . import views
 
 urlpatterns=[
     path('',views.initalhome),
-    path('news/',views.home)
+    path('news/',views.home),
+    path('search/', views.search, name='search'),
 ]
